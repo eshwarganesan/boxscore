94979363e19ee1f42cb823101a26b1fdb8ac1f87
Rajamohan Ganesan <eganesan@rajamohans-mbp.wireless.concordia.ca>
2023-09-27 14:20:01 -0400

    added post method to save boxscore to mongodb database

---
+++ server/server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const config = require("./database");
+const cors = require("cors");
+
+const app = express();
+
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // Replace with your frontend's URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Enable cookies and authentication headers (if needed)
+};
+
+
+let db;
+
+mongoClient
+    .connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((client) => {
+        db = client.db('boxscores'); // Assign the database connection to the "db" variable
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    req.db = db; // Attach the database connection to the request object
+    console.log("Time:", Date.now(), "Request:", req, "Result:", res);
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.send("hello");
+});
+
+app.post("/save-box-score", (req, res) => {
+    console.log("Saving box score");
+    const collection = req.db.collection("boxScores");
+    const boxScoreData = req.body;
+    try{
+        collection.insertOne(boxScoreData);
+        console.log("Box score saved successfully");
+        res.status(200).json({ message: "Box score saved successfully" });
+    }
+    catch(err){
+        console.log("Failed to save box score");
+        console.log(err);
+        res.status(500).json({ message: "Faled to save box score", error: err})
+    }
+    
+
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
--- src/pages/App.js
+++ src/pages/App.js
@@ -9,10 +9,13 @@ import { auth } from "../firebase";
 import { useEffect } from "react";
 import ProtectedRoute from "./ProtectedRoute";
 import Profile from "./Profile";
+import { useSelector } from "react-redux";
+import axios from 'axios'
 
 function App() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [userSignedIn, setUserSignedIn] = useState(false);
+    const boxScore = useSelector((state) => state.boxScore);
 
     const handleOpenModal = () => {
         setIsModalOpen(true);
@@ -25,6 +28,18 @@ function App() {
     const handleSignOut = async () => {
         await auth.signOut();
     };
+
+    const handleSave = () => {
+        axios.post('http://localhost:3001/save-box-score', {boxScore})
+            .then(res => {
+                console.log(res);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
+
+        
+    }
 
     useEffect(() => {
         // Update the userSignedIn state based on the authentication status
@@ -44,6 +59,7 @@ function App() {
             <div className="Header">
                 {userSignedIn ? (
                     <div>
+                        <button onClick={handleSave}>Save box score</button>
                         <Link className="btn btn-primary" to="/profile">Profile</Link>
                         <button onClick={handleSignOut}>Sign Out</button>
                     </div>
