a357968394f8d7d7668f000ea22d6196e23cbe2f
Rajamohan Ganesan <eganesan@Rajamohans-MacBook-Pro.local>
2024-01-18 18:02:44 -0500

    added profile page that lists all box scores from mongodb and redirects to scoreboard with loaded data on click

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setBoxScore } from "../boxSlice.js";
+import axios from "axios";
+import firebase from "../firebase.js";
+
 function Profile() {
-  return(<div></div>);
+    const [boxScores, setBoxScores] = useState([]);
+    const [token, setToken] = useState(null);
+    const uid = firebase.auth.currentUser.uid;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchBoxScores = async () => {
+            try {
+                firebase.auth.currentUser.getIdToken().then((token) => {
+                    setToken(token);
+                });
+                const response = await axios.get(
+                    `http://localhost:3001/box-scores/${uid}`,
+                    {
+                        headers: { Authorization: token },
+                    }
+                );
+                setBoxScores(response.data);
+            } catch (error) {
+                console.error("Error fetching box scores:", error);
+            }
+        };
+
+        fetchBoxScores();
+    }, [uid, token]);
+
+    const loadScore = (id) => {
+      console.log(boxScores[id]);
+      dispatch(setBoxScore(boxScores[id].boxScore))
+    };
+    return (
+        <div>
+            <ul>
+                {boxScores.map((boxScore, index) => (
+                    <li key={index} onClick={() => loadScore(index)}>
+                        <Link to="/">
+                            {boxScore.boxScore.Totals.Home.name} vs{" "}
+                            {boxScore.boxScore.Totals.Away.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Profile;
